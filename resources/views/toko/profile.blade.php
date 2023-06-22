ef005af7bf18f4ddc081137fe102f9524c94fd70
DimasAthaHandria <dimashandria07@gmail.com>
2023-06-22 18:54:12 +0700

    tugas dan praktikum pertemuan 8-12

---
+++ resources/views/toko/profile.blade.php
@@ -0,0 +1,23 @@
+@extends('template/main')
+
+@section('content')
+<div class="row">
+    <div class="col-md-8">
+        <h1>Toko Sepatu HNN</h1>
+        <P>Selamat datang di toko sepatu kami! Kami adalah destinasi utama untuk semua kebutuhan sepatu Anda. Dengan berbagai macam gaya, merek, dan kualitas terbaik, kami menawarkan pengalaman berbelanja sepatu yang tak tertandingi.
+
+Di toko kami, Anda akan menemukan koleksi sepatu yang luas, mulai dari sepatu olahraga yang nyaman, sepatu formal yang elegan, sepatu kasual yang trendi, hingga sepatu outdoor yang tahan lama. Kami bekerja sama dengan merek-merek ternama dan terpercaya untuk menyediakan sepatu berkualitas tinggi yang sesuai dengan selera dan kebutuhan Anda.
+
+Kami sangat menghargai kepuasan pelanggan, dan staf kami yang ramah dan berpengetahuan akan dengan senang hati membantu Anda menemukan sepatu yang sempurna. Dengan pelayanan yang cepat dan efisien, kami berkomitmen untuk memberikan pengalaman berbelanja yang menyenangkan dan mudah bagi semua pelanggan.
+
+Selain koleksi sepatu yang lengkap, kami juga menawarkan berbagai aksesori sepatu, seperti kaos kaki, sol dalam, dan produk perawatan sepatu, untuk memastikan kebutuhan Anda terpenuhi secara menyeluruh.
+
+Kami selalu berupaya memberikan harga yang kompetitif dan penawaran spesial untuk membuat pengalaman berbelanja Anda lebih terjangkau dan memuaskan. Kami juga menjaga kualitas produk kami dengan cermat, sehingga Anda dapat yakin bahwa setiap sepatu yang Anda beli dari toko kami akan bertahan lama dan memberikan kenyamanan maksimal.
+
+Jadi, kunjungilah toko sepatu kami sekarang dan temukan sepatu yang tepat untuk gaya dan kebutuhan Anda. Kami berharap dapat melayani Anda dengan baik dan membuat Anda kembali lagi untuk berbelanja di toko kami. Terima kasih telah memilih kami sebagai tujuan belanja sepatu Anda!</P>
+    </div>
+    <div class="col-md-4">
+        <img src="/public/img/toko.jpg" class="w-100" alt="foto profile">
+    </div>
+</div>
+@endsection
